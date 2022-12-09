28b3e7afe9ec925cbf924fd9b07920622194e249
saurav kattel <kattelsaurav80@gmail.com>
2022-12-09 17:10:44 +0545

    fixed cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require ("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 app.use(express.json())
 app.use("/api/note", require("./src/note"));
 app.use("/api/user", require("./src/user"));
+app.use(cors());
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://Saurav:connectBabe@mydb.wqgpv.mongodb.net/?retryWrites=true&w=majority",()=>{
   console.log("successfully connected")
