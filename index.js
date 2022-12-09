5a9dd418200028d678c0211e2582550b012cf673
saurav kattel <kattelsaurav80@gmail.com>
2022-12-09 17:41:54 +0545

    fixed cors 2

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const port = 4000;
 app.use(express.json())
 app.use("/api/note", require("./src/note"));
 app.use("/api/user", require("./src/user"));
-app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb+srv://Saurav:connectBabe@mydb.wqgpv.mongodb.net/?retryWrites=true&w=majority",()=>{
   console.log("successfully connected")
