f6bd470b75f2d44ac6849be06ec2cf50226baa53
saurav kattel <kattelsaurav80@gmail.com>
2022-12-09 19:52:18 +0545

    fustration level 98% with fuck you in cors may be fixed 22

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require ("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-app.use(cors());
+
 const port = 4000;
+app.use(cors());
 app.use(express.json())
 app.use("/api/note", require("./src/note"));
 app.use("/api/user", require("./src/user"));
 
 mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://Saurav:connectBabe@mydb.wqgpv.mongodb.net/?retryWrites=true&w=majority",()=>{
+mongoose.connect("mongodb+srv://Saurav:connectBabe@mydb.wqgpv.mongodb.net/?retryWrites=true&w=majority", ()=> {
   console.log("successfully connected")
 })
 
-app.listen(port,()=>{
+app.listen(port, ()=> {
   console.log("listening on " + port)
 })
