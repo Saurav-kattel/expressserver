6de910fbf70bba66ff038db646465a91a2c7f599
saurav kattel <kattelsaurav80@gmail.com>
2022-12-17 10:49:29 +0545

    new cors origin

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ const port = 4000;
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:3000"
+    origin: {
+      "http://localhost:3000",
+     "https://reactnotesfrontend.vercel.app"
+      
+    }
   })
 );
 app.use("/api/note", require("./src/note"));
